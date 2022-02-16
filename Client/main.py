cf6392054c735ef2d3fb060942dbf57d5ae9be41
SimchaTeich <sim.teich@gmail.com>
2022-02-16 03:04:44 +0200

    minimal idea for client from 'the user eyes'

---
+++ Client/main.py
@@ -0,0 +1,17 @@
+import socket
+from threading import Thread
+#import logic
+
+def main():
+    
+    # TODO: connect to server
+    
+    username = input("Hey, enter your username: ")
+    
+    if is_valid_username(username):
+        # TODO: print menu
+        
+        # TODO: while choice is valid and not exit,
+        #       send it to logic unit.
+        
+        
