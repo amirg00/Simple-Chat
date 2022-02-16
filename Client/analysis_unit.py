9ca7670568496da4dfc82c06904e465003e1c16e
SimchaTeich <sim.teich@gmail.com>
2022-02-16 06:05:16 +0200

    create nalysis unit and implements 'confirm analysis' function

---
+++ Client/analysis_unit.py
@@ -0,0 +1,94 @@
+#---------PROTOCOL_MESSAGES--------#
+# PROTOCOL CODES                   #
+GET = "1"                          #
+CONFIRM = "2"                      #
+UPDATE = "3"                       #
+ERROR = "4"                        #
+                                   #
+# PROTOCOL TYPE CODES              #
+CONNECT_CODE = "00"                #
+DISCONNECT_CODE = "01"             #
+USERS_LIST_CODE = "02"             #
+SEND_MSG_CODE = "03"               #
+SEND_BROADCAST_MSG_CODE = "04"     #
+#----------------------------------#
+
+def create_gets_msg(type_code, src = "", dst = "", msg = ""):  
+    pass
+    
+def analysis_confirms_msg(type_code, msg):
+    if type_code is CONNECT_CODE:
+        username_len = int(msg[:2])
+        msg = msg[2:]
+        username = msg[:username_len]    
+        print(f"Connect to server sucsses! your username is {username}")
+        return True
+    
+    if type_code is DISCONNECT_CODE:
+        print("Logout success.")
+        return True
+    
+    if type_code is USERS_LIST_CODE:
+        users_exist = int(msg[:1])
+        msg = msg[1:]
+        if users_exist:
+            users_amount = msg[:1]
+            msg = msg[1:]
+            print("Users in chat: ")
+            for i in range(users_amount):
+                username_len = int(msg[:2])
+                msg = msg[2:]
+                username = msg[:username_len]
+                print(username)
+            return True
+        else:
+            print("You are the only person in the chat!")
+            return False
+    
+    if type_code is SEND_MSG_CODE:
+        print("Your message was sent!")
+        return True
+    
+    if type_code is SEND_BROADCAST_MSG_CODE:
+        users_exist = int(msg[:1])
+        msg = msg[1:]
+        if users_exist:
+            users_amount = msg[:1]
+            msg = msg[1:]
+            print("Users was get your message: ")
+            for i in range(users_amount):
+                username_len = int(msg[:2])
+                msg = msg[2:]
+                username = msg[:username_len]
+                print(username)
+            return True
+        else:
+            print("You are the only person in the chat!")
+            return False    
+
+def analysis_updates_msg(type_code, msg):
+    pass
+    
+def analysis_errors_msg(type_code, msg):
+    pass
+
+def analysis_msg_main(msg):
+    # get msg code
+    code = msg[:1]
+    msg = msg[1:]
+    
+    # get type of msg code
+    type_code = msg[:2]
+    msg = msg[2:]
+    
+    if code is GET:
+        analysis_gets_msg(type_code, msg)
+    elif code is CONFIRM:
+        analysis_confirms_msg(type_code, msg)
+    elif code is UPDATE:
+        analysis_updates_msg(type_code, msg)
+    elif code is ERROR:
+        analysis_errors_msg(type_code, msg)
+    else:
+        printf(f"Woops, error. this is msg make it {code + type_code + msg}")
+        exit()
--- Client/logic.py
+++ Client/logic.py
@@ -7,42 +7,6 @@ EXIT_OPTION = 0
 MIN_OPTION = 0
 MAX_OPTION = 3
 
-#---------PROTOCOL_MESSAGES--------#
-# PROTOCOL CODES                   #
-GET = "1"                          #
-CONFIRM = "2"                      #
-UPDATE = "3"                       #
-ERROR = "4"                        #
-                                   #
-# PROTOCOL TYPE CODES              #
-CONNECT_CODE = "00"                #
-DISCONNECT_CODE = "01"             #
-USERS_LIST_CODE = "02"             #
-SEND_MSG_CODE = "03"               #
-SEND_BROADCAST_MSG_CODE = "04"     #
-#----------------------------------#
-
-def analysis_msg_main(msg):
-    # get msg code
-    code = msg[:1]
-    msg = msg[1:]
-    
-    # get type of msg code
-    type_code = msg[:2]
-    msg = msg[2:]
-    
-    if code is GET:
-        analysis_get_msg(type_code)
-    elif code is CONFIRM:
-        analysis_confirm_msg(type_code)
-    elif code is UPDATE:
-        analysis_updates_msg(type_code)
-    elif code is ERROR:
-        analysis_confirm_msg(type_code)
-    else:
-        printf(f"Woops, error. this is msg make it {code + type_code + msg}")
-        exit()
-
 
 def listen_to_server()
     pass
